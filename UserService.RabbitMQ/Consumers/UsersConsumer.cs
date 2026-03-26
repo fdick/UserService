6f07b9154988c32f60e497559e09fd689efd2a06
awanes <qodux.corp@gmail.com>
2026-03-27 01:16:53 +0300

    feat: add java plugin keycloak-to-rabbit

---
+++ UserService.RabbitMQ/Consumers/UsersConsumer.cs
@@ -0,0 +1,15 @@
+﻿using MassTransit;
+
+namespace UserService.RabbitMQ.Consumers
+{
+    public class UsersConsumer : IConsumer<UserMQEvent>
+    {
+        public Task Consume(ConsumeContext<UserMQEvent> context)
+        {
+            var b = context.ReceiveContext.GetBody();
+            return Task.CompletedTask;
+        }
+    }
+
+    public record UserMQEvent(Guid id, string name, string lastname, string nickname, string email);
+}
--- UserService/Program.cs
+++ UserService/Program.cs
@@ -1,5 +1,8 @@
+using MassTransit;
 using UserService.Application.Services;
 using UserService.Core.Abstractions;
+using UserService.RabbitMQ.Consumers;
+using UserService.RabbitMQ.Definitions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +12,37 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddHttpClient();
 builder.Services.AddScoped<IKeycloakService,  KeycloakService>();
+
+builder.Services.AddMassTransit(busConf =>
+{
+    busConf.SetKebabCaseEndpointNameFormatter();
+
+    busConf.AddConsumer<UsersConsumer>();
+
+    busConf.UsingRabbitMq((ctx, configurator) =>
+    {
+        configurator.Host(new Uri(builder.Configuration["MessageBroker:Host"]), h =>
+        {
+            h.Username(builder.Configuration["MessageBroker:Username"]);
+            h.Password(builder.Configuration["MessageBroker:Password"]);
+        });
+
+        //configurator.ConfigureEndpoints(ctx);
+
+        // Просто подключаемся к существующей очереди
+        configurator.ReceiveEndpoint(builder.Configuration["MessageBroker:Users-queue"], e =>
+        {
+            // Отключаем автоматическое создание всего
+            e.ConfigureConsumeTopology = false;
+
+            // Настраиваем чтение JSON
+            e.ClearSerialization();
+            e.UseRawJsonSerializer();
+
+            e.ConfigureConsumer<UsersConsumer>(ctx);
+        });
+    });
+});
 
 var app = builder.Build();
 
